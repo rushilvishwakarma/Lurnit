6f64b27c11b68b7ce86d7230e98e4a75885c0119
Rushil <rushil.edu@gmail.com>
2024-07-17 02:25:24 +0530

    Disabled text selection

--- src/components/panel/footer.tsx
+++ src/components/panel/footer.tsx
@@ -5,7 +5,7 @@ export function Footer() {
     <div className="z-20 w-full bg-muted/50 backdrop-blur dark:bg-muted/30">
       <div className="mx-4 md:mx-8 flex h-14 items-center">
         <p className="text-xs md:text-sm leading-loose text-muted-foreground text-left">
-          Hello world :P
+        2024 Lurnit
         </p>
       </div>
     </div>
